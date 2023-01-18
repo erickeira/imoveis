7ffa1597915f822df086eb659e08a0d0bcd1b705
Erick Almeida <erick.almeida.ricardo@gmail.com>
2023-01-17 21:41:56 -0400

    alteracao de cores

--- components/sliderFilter/index.js
+++ components/sliderFilter/index.js
@@ -2,7 +2,6 @@ import React, { useState} from 'react'
 import styles from './sliderfilter.module.scss'
 import Link from 'next/link'
 import {AiOutlineSearch} from 'react-icons/ai'
-import { Fade } from 'react-slideshow-image';
 import { loaderImg } from '../../utils'
 import Image from 'next/image'
 import InputSelect from '../inputSelect'
@@ -13,45 +12,9 @@ import FormBusca from '../formBusca';
 export default function SliderFilter(){
 
 
-    const Slide = () => {
-        const images = [
-            "/slide/img01.jpg"
-            // "/slide/img02.jpg",
-            // "/slide/img03.jpg",
-        ];
-      
-        return (
-            <Fade
-                transitionDuration={1000}
-                arrows={false}
-            >
-                {
-                    images.map(image => {
-                        return(
-                            <div className="each-slide">
-                                <div className={styles.containerImagemSlide}>
-                                    <Image
-                                        loader={loaderImg}
-                                        src={image}
-                                        alt="Casa"
-                                        width={1400}
-                                        height={600}
-                                        unoptimized
-                                    />
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-                
-            </Fade>
-        );
-      };
-
     return(
         <div className={styles.container}>
             <div  className={`${styles.containerImgs1} slide-container`}>
-                {/* <Slide/> */}
             </div>
             <div className={`containerTela  ${styles.containerDentro}`}>
                 <FormBusca/>
