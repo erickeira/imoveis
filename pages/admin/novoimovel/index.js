6a2a4a0c225484a3e9ce0635f5669110b94e7435
Erick Almeida <erick.almeida.ricardo@gmail.com>
2023-02-02 20:52:47 -0400

    teste grid imagens

--- pages/admin/novoimovel/index.js
+++ pages/admin/novoimovel/index.js
@@ -15,6 +15,7 @@ export default function Novoimovel(){
         finalidade: '',
         tipo: '',
         bairro: '',
+        rua: '',
         titulo: '',
         areaconstruida: '',
         areatotal: '',
